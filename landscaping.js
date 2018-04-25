b6648015ad71598fdeffa2cffdc246409461a2f8
ELeonJohnson <enochjohnsonleon@gmail.com>
2018-04-25 06:31:43 -0400

    Worked on Home Page

---
+++ landscaping.js
@@ -0,0 +1,21 @@
+var myVideo = document.getElementById("video1");
+
+function play() {
+	myVideo.play();
+}
+
+function pause() {
+	myVideo.pause();
+}
+
+function makeBig() {
+	myVideo.width = 560;
+}
+
+function makeSmall() {
+	myVideo.width = 320;
+}
+
+function makeNormal() {
+	myVideo.width = 420;
+}
